d7ce90ad6a3bb8a40b54c4cfab718df8bf74bb6e
clement-masson <masson.cle@gmail.com>
2016-06-21 12:58:54 +0200

    Edit README, remove build folders in plugin folders

--- fix_gazebo_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Edit gazebo's setup so that the following env variable changes are not reset at Gazebo's launch
-gazebo_setup_path=$(pkg-config --variable=prefix gazebo)/share/gazebo/setup.sh
-python fix_gazebo_setup.py $gazebo_setup_path
