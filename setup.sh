9376c827faf99d9aa5d52e5cfd62b8645a25aed3
clement-masson <masson.cle@gmail.com>
2016-06-21 10:56:47 +0200

    add plugin directories to the CMakeList of the catkin package, change setup.sh accordingly. From now, fix_gazebo_setup should not be needed any more

--- setup.sh
+++ setup.sh
@@ -2,16 +2,6 @@
 
 package_path=$(rospack find arm_scenario_simulator)
 
-# Gather the package's plugin libraries into a unique lib directory
-mkdir -p $package_path/lib
-rm -f $package_path/lib/*.so
-ln -f $package_path/models/*/plugins/build/*.so $package_path/lib/
-echo "Found plugin libraries :"
-ls $package_path"/lib"
-
 # Set GAZEBO env variables and 
 export GAZEBO_MODEL_PATH=$package_path/models:$GAZEBO_MODEL_PATH
-export GAZEBO_PLUGIN_PATH=$package_path/lib:$GAZEBO_PLUGIN_PATH
-
 echo "GAZEBO_MODEL_PATH = "$GAZEBO_MODEL_PATH
-echo "GAZEBO_PLUGIN_PATH = "$GAZEBO_PLUGIN_PATH
