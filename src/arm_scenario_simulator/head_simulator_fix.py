e0e1fb10a309961d02d0f6029e9ed477e8749822
clement-masson <masson.cle@gmail.com>
2016-09-27 09:57:37 +0200

    Ajout doc, deplacement de head_controller_fix

--- src/arm_scenario_simulator/head_simulator_fix.py
+++ src/arm_scenario_simulator/head_simulator_fix.py
@@ -1,16 +1,13 @@
 '''
 This scripts exists because baxter's gazebo simulator seems to not support the speed argument given to head.set_pan.
-Indeed, th head turns almost instantenously to the pan command given to set_pan, indepenently to the speed command given.
+Indeed, the head turns almost instantenously to the pan command given to set_pan, indepenently to the speed command given.
 '''
+import os
+import subprocess
 
 import rospy
-import roslaunch
 from baxter_interface import Head
-
-from baxter_core_msgs.msg import (
-   HeadPanCommand,
-   HeadState,
-)
+from baxter_core_msgs.msg import HeadPanCommand
 
 try:
     rospy.wait_for_service('/cameras/list', timeout=1) # test if the service is available
@@ -19,6 +16,7 @@ except rospy.ROSException: # if not, it is probably cause we're using the simula
     rospy.loginfo("You are using the simulator")
 
     fix_publisher = rospy.Publisher('/robot/head/command_head_pan_simulator_fix',  HeadPanCommand, queue_size=10)
+    
     def move_to_pan(self, angle, speed, blocking = False, margin=0.03, timeout=5):
         start = rospy.get_time()
         msg = HeadPanCommand(angle, speed, True)
@@ -27,17 +25,17 @@ except rospy.ROSException: # if not, it is probably cause we're using the simula
             rate = rospy.Rate(10)
             while abs(self.pan() - angle)>margin:
                 if (rospy.get_time()-start)>timeout:
-                    rospy.logerr('Could not reach the target angle (+/- '+margin+' rad) within '+str(timeout)+ 'sec')
+                    rospy.logerr('Could not reach the target angle (+/- '+str(margin)+' rad) within '+str(timeout)+ 'sec')
                     break
                 rate.sleep()
     Head.move_to_pan = move_to_pan #attach a new method to Head class
 
 
-    launch = roslaunch.scriptapi.ROSLaunch()
-    launch.start()
-    head_controler_fix_node = launch.launch(roslaunch.core.Node('arm_scenario_simulator', 'head_controller_fix'))
-
-    def clean_up():
-        head_controler_fix_node.stop()
-        launch.stop()
-    rospy.on_shutdown(clean_up)
+    try: # look if the node already exists
+        rospy.wait_for_service('/head_controller_simulator_fix', 5.0)
+    except:
+        path = os.path.join(os.path.dirname(__file__), 'head_controller_simulator_fix')
+        head_controler_fix_node = subprocess.Popen(['python', path])
+        def clean_up():
+            head_controler_fix_node.kill()
+        rospy.on_shutdown(clean_up)
