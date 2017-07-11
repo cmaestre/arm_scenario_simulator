ee58aace4e0b91d8930fdf576b831c01801c426c
Carlos Maestre <maestre@isir.upmc.fr>
2017-07-11 14:07:09 +0200

    Light can only change state when pressed

--- scripts/button_box_setup.py
+++ scripts/button_box_setup.py
@@ -84,9 +84,7 @@ class Environment:
         
 
     def del_objects(self):
-        self.objects['light_table0'].turn_off()
-        self.objects['light_table1'].turn_off()
-        self.objects['light_table2'].turn_off()        
+        self.objects['light_table0'].turn_off()      
         for obj in self.objects.keys(): 
             self.objects[obj].delete()        
 
@@ -94,49 +92,22 @@ class Environment:
         if obj: self.objects[obj.gazebo_name] = obj
 
     def init(self):
-        ''' Create handles to (and spawn) simulated object in Gazebo'''        
-        self.add_object( arm_sim.Lever('lever1').spawn( 
-            Point(x=0.5, y=0.6, z=0.76), orientation=axis_to_quat([0,0,1], math.pi/2) ) )
+        ''' Create handles to (and spawn) simulated object in Gazebo'''
         self.add_object( arm_sim.Button('button1').spawn( 
             Point(x=0.5, y=0.3, z=0.76) ) )
-        self.add_object( arm_sim.Button('button2').spawn( 
-            Point(x=0.5, y=0, z=0.76) ) )
-
         self.add_object( arm_sim.GazeboObject('cube1').spawn(
             'DREAM_cube', Point(x=0.5, y=0.45, z=0.8) ))
-#        self.add_object( arm_sim.GazeboObject('cube2').spawn(
-#            'DREAM_cube', Point(x=0.4, y=0.2, z=0.78) ))
-#        self.add_object( arm_sim.GazeboObject('cylinder1').spawn(
-#            'DREAM_cylinder',  Point(x=0.75, y=0.05, z=0.79) ))
 
         # the following objects are not spawn, cause already present in the world before this script is run
-        self.add_object( arm_sim.Pocket('table/pocket') )
-        self.add_object( arm_sim.Light('light_table0', color=[0,0,255]) )
-        self.add_object( arm_sim.Light('light_table1', color=[255,0,0]) )
-        self.add_object( arm_sim.Light('light_table2', color=[0,255,0]) )
+        self.add_object( arm_sim.Light('light_table0', color=[0,255,0]) )
 
         # to let the publishers notify the master, so that the following commands are not discarded
         rospy.sleep(1)
         # Initialize objects attributes (color) and lights states
-        self.objects['light_table0'].set_light_state(on = False, force = True) # force=True ensures sending a ros message to gazebo to set the gazebo_object's color, no matter what the current python object state is
-        self.objects['light_table1'].set_light_state(on = False, force = True) # when force is False or ommited, the python object only publish a ros message if it considers it is necessary
-        self.objects['light_table2'].set_light_state(on = False, force = True) # it is recommended to force at initialization time to ensure having a consistent state
-
-        self.objects['lever1'].set_base_color(rgba = [0,0,255])
-        self.objects['lever1'].set_lever_color(rgba = [0,0,255])
-
-        self.objects['button1'].set_base_color(rgba = [255,0,0])
-        self.objects['button1'].set_button_color(rgba = [255,0,0])
+        self.objects['light_table0'].set_light_state(on = False, force = True)
+        self.objects['button1'].set_base_color(rgba = [0,255,0])
+        self.objects['button1'].set_button_color(rgba = [0,255,0])
         
-        self.objects['button2'].set_base_color(rgba = [0,255,0])
-        self.objects['button2'].set_button_color(rgba = [0,255,0])        
-
-#        self.objects['cube1'].set_color(rgba = [230,220,10])
-#        self.objects['cube2'].set_color(rgba = [200,20,20])
-#        self.objects['cylinder1'].set_color(rgba = [50,50,220])
-
-        
-
     def run(self):
         rate = rospy.Rate(self.rate)
         while not rospy.is_shutdown():
@@ -146,27 +117,13 @@ class Environment:
 
     def rules(self):            
         objects = self.objects
-
-        objects['light_table0'].set_light_state( objects['lever1'].is_pushed() )
         
-        if not objects['lever1'].is_pushed():
-            objects['light_table0'].turn_off()
-            objects['light_table1'].turn_off()
-            objects['light_table2'].turn_off()
-        else:  
-            if objects['button1'].is_pressed():
-                objects['light_table1'].turn_on() if not objects['light_table1'].is_on() else \
-                objects['light_table1'].turn_off()
-                time.sleep(1)
-                
-            if objects['light_table1'].is_on() and objects['button2'].is_pressed():            
-                objects['light_table2'].turn_on() if not objects['light_table2'].is_on() else \
-                objects['light_table2'].turn_off()                
-                time.sleep(1)
-                    
-        # to access an objects pose : 
-#        print(objects['lever1'].get_state().pose)
+        print(objects['button1'].is_pressed(), objects['button1'].state_changed())
 
+        if objects['button1'].is_pressed() and objects['button1'].state_changed():
+            objects['light_table0'].turn_on() if not objects['light_table0'].is_on() else \
+            objects['light_table0'].turn_off()
+            time.sleep(5)
 
 def main():
     rospy.init_node("DREAM_environment_demo")
--- src/arm_scenario_simulator/button.py
+++ src/arm_scenario_simulator/button.py
@@ -1,4 +1,5 @@
 import rospy
+import time
 from .gazeboObject import GazeboObject
 from std_msgs.msg import ColorRGBA, Int8
 from arm_scenario_simulator.msg import MaterialColor, Int8Stamped
@@ -9,17 +10,25 @@ class Button(GazeboObject):
 
     def __init__(self, name):
         GazeboObject.__init__(self, name)
+        self._pressed_before = False        
         self._pressed = False
+        self._changed = False
         rospy.Subscriber("/"+name+"/is_pressed", Int8Stamped, self.update_state, queue_size=1)
 
     def spawn(self, position, orientation = None, **extra):
         return GazeboObject.spawn(self, 'DREAM_push_button', position, orientation, **extra)
 
-    def update_state(self, message):
+    def update_state(self, message):        
+        self._pressed_before = self._pressed
+        time.sleep(0.05)
         self._pressed = message.data==1
+        self._changed = self._pressed_before != self._pressed        
 
     def is_pressed(self):
         return self._pressed
+        
+    def state_changed(self):
+        return self._changed
         
     def set_base_color(self, rgba):
         self.set_color(rgba,'base')
