8046e9915238e88e2d024d8c6f5024dc31686851
Carlos Maestre <maestre@isir.upmc.fr>
2017-07-11 11:35:08 +0200

    New restart_environment and get_object_pose services

--- scripts/button_box_setup.py
+++ scripts/button_box_setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import argparse
 import sys
 import os
@@ -12,7 +14,10 @@ import rospy
 import arm_scenario_simulator as arm_sim
 
 from geometry_msgs.msg import Point, Quaternion
-from std_msgs.msg import Empty
+from arm_scenario_simulator.srv import *
+#from std_msgs.msg import Empty
+from gazebo_msgs.srv import GetModelState
+import traceback
 
 
 def axis_to_quat(axis, angle):
@@ -24,11 +29,58 @@ class Environment:
     def __init__(self):
         self.objects = {}
         self.rate = 100
-        rospy.Subscriber('/environment/reset', Empty, self.reset_callback, queue_size = 1)
+#        rospy.Subscriber('/environment/reset', Empty, self.reset_callback, queue_size = 1)
+        self.reset_service = rospy.Service('/env/restart_environment', 
+                                            RestartWorld, 
+                                            self.reset_callback)
+        self.get_object_pose_service = rospy.Service('/env/get_object_state', 
+                                            GetObjectState, 
+                                            self.get_object_pose_callback)
+    
+    def get_object_pose_callback(self, req):
+        obj_name = req.object_name
+        try:
+            client = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
+            obj_pose = client(obj_name, 'world')
+            return GetObjectStateResponse([obj_pose.pose.position.x,
+                                           obj_pose.pose.position.y,
+                                           obj_pose.pose.position.z - 0.785])
 
-    def reset_callback(self, message):
-        self.del_objects()
-        self.init()
+        except AttributeError:
+            traceback.print_exc()
+        except:
+            print('get_object_pose_callback - Error getting pose of object', obj_name)
+
+    def reset_callback(self, req):
+        if req.option == 'setup':
+            self.del_objects()
+            self.init()
+        elif req.option == 'object':
+            obj_name = req.model_name
+            self.objects[obj_name].delete()
+            req_x = req.model_pos_x
+            req_y = req.model_pos_y
+            req_z = req.model_pos_z
+            if 'button' in obj_name:
+                self.add_object( arm_sim.Button(obj_name).spawn( 
+                    Point(x=req_x, y=req_y, z=req_z + 0.76) ) )
+            elif 'lever' in obj_name:
+                self.add_object( arm_sim.Lever(obj_name).spawn( 
+                    Point(x=req_x, y=req_y, z=req_z + 0.76), orientation=axis_to_quat([0,0,1], math.pi/2) ) )
+            elif 'cube' in obj_name:
+                self.add_object( arm_sim.GazeboObject('cube1').spawn('DREAM_cube',
+                    Point(x=req_x, y=req_y, z=req_z + 0.76) ))
+            elif 'cylinder' in obj_name:
+                self.add_object( arm_sim.GazeboObject('cylinder1').spawn('DREAM_cylinder',
+                    Point(x=req_x, y=req_y, z=req_z + 0.76) ))
+            else:
+                print('reset_callback - Object name to spawn unknown:', obj_name)
+                return False
+        else:
+            print('reset_callback - Option name unknown:', req.option)
+            return False
+        
+        return True
         
 
     def del_objects(self):
